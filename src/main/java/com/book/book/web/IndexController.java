d67eed5257545d1742ec528c63cb6b08c81cc0d3
leeminjung1 <vipminjung@gmail.com>
2021-09-07 16:22:31 +0900

    add OAuth2.0

--- src/main/java/com/book/book/web/IndexController.java
+++ src/main/java/com/book/book/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.book.book.web;
 
+import com.book.book.config.auth.LoginUser;
+import com.book.book.config.auth.dto.SessionUser;
 import com.book.book.service.posts.PostsService;
 import com.book.book.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -15,8 +19,11 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index"; // index.mustache
     }
 
